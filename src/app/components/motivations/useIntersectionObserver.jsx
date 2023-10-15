a71255b87675f296f9796512edc933fe2f677586
Mr Robot <145590545+elisioMassaqui@users.noreply.github.com>
2023-10-15 14:55:08 +0100

    Add files via upload

---
+++ src/app/components/motivations/useIntersectionObserver.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from "react";
+
+// Hook personalizado para observar a visibilidade de um elemento
+export function useIntersectionObserver(options) {
+  const [isVisible, setIsVisible] = useState(false);
+  const targetRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      options
+    );
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, [options]);
+
+  return [targetRef, isVisible];
+}
