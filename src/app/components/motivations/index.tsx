f256bf8f12a25699affb4b4fabe5ed2a6813644f
Mr Robot <145590545+elisioMassaqui@users.noreply.github.com>
2023-10-15 17:23:42 +0100

    Update index.tsx

--- src/app/components/motivations/index.tsx
+++ src/app/components/motivations/index.tsx
@@ -2,22 +2,23 @@ import React, { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import ButtonLink from "../buttonLink";
 import { MOTIVATIONS } from "./data";
-import { useIntersectionObserver } from "./useIntersectionObserver";
+import { useIntersectionObserver } from "./useIntersectionObserver"; // Importe o hook de observação de visibilidade
 
 function Motivations({ className, ...rest }: any) {
   const [typedText, setTypedText] = useState("");
-  const textToType =
-    "Somos guiados por um conjunto de motivações que nos ajudam a efetivamente alcançar a nossa missão.";
-  const typingSpeed = 30;
+  const textToType = "Somos guiados por um conjunto de motivações que nos ajudam a efetivamente alcançar a nossa missão.";
+  const typingSpeed = 30; // Velocidade da digitação em milissegundos
 
+  // Use o hook de observação de visibilidade
   const [targetRef, isVisible] = useIntersectionObserver({
     root: null,
     rootMargin: "0px",
-    threshold: 0.5,
+    threshold: 0.5, // Quando pelo menos 50% do componente está visível
   });
 
   useEffect(() => {
     if (isVisible) {
+      // Inicie a animação de digitação apenas quando o componente estiver visível
       let currentText = "";
       let currentIndex = 0;
 
@@ -43,14 +44,13 @@ function Motivations({ className, ...rest }: any) {
       {...rest}
     >
       <div
-        ref = {targetRef}
-        className="w-full max-w-[1216px] py-10 flex flex-col xl:flex-col gap-7"
+        className="w-full max-w-[1216px] py-44 flex justify-between flex-col xl:flex-row gap-7"
       >
-        <div className="max-w-[1200px] items-center flex flex-col gap-3">
+        <div className="max-w-[490px] flex flex-col gap-3">
           <h2 className="md:text-5xl text-3xl font-bold leading-[130%] text-white">
             Nossas Motivações
           </h2>
-          <p className="max-w-[1200px] md:text-lg text-sm font-medium leading-[10%] text-white">
+          <p className="md:text-xl text-sm font-medium leading-[150%] text-white">
             {typedText}
           </p>
           <ButtonLink
@@ -65,10 +65,10 @@ function Motivations({ className, ...rest }: any) {
             {MOTIVATIONS.map(({ id, title, body }) => (
               <li key={id}>
                 <div className="bg-[#262626] p-8 flex flex-col gap-2 rounded-2xl">
-                  <h2 className="text-2xl text-white font-bold leading-[150%]">
+                  <h2 className="text-2xl text-[#F09E00] font-bold leading-[150%]">
                     {title}
                   </h2>
-                  <p className="text-base text-[#a3a3a3] font-medium leading-[150%]">
+                  <p className="text-base text-[#ffa] font-medium leading-[150%]">
                     {body}
                   </p>
                 </div>
