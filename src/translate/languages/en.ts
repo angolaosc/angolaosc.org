b8fd4730f317251f879db897791cd13745b02864
Afranioalves <afranio777alves@gmail.com>
2023-10-16 10:26:58 +0100

    feat: Add translate config and language

---
+++ src/translate/languages/en.ts
@@ -0,0 +1,53 @@
+const messages = {
+    en: {
+        translations: {
+            titles: {
+                app: 'My application: {{name}}'
+            },
+            heading: {
+                info: 'Take part now in the first face-to-face event of the Angola Open Source Commnunity in Luanda.',
+                smallText: 'A small text'
+            },
+            nav: {
+               link_1 : 'Home',
+               link_2 : 'Mentorship Program',
+               link_3 : 'Team',
+               link_4 : 'Event',
+               link_5 : 'Motivations',
+               button : 'Donate'
+            },
+            section_1:{
+                title :`Creating a significant impact, on technological innovation in the country.`,
+                description : 'Our mission is to promote the development and adoption of Free and Open-source Software (FOSS) in Angola.',
+                button : 'Join the community',
+                link : 'Learn more',
+                info :'Join 200+ members',
+                bloom_1 : 'Free Software',
+                bloom_2 : 'Collaboration'
+
+             },
+             marquee:{
+                text_1 : "Community",
+                text_2 : 'Free Software',
+                text_3 : 'Collaboration'
+             },
+             section_2:{
+                title : "Our main goals",
+                card_1 :{
+                    title : 'Encouraging a free software culture.',
+                    description : 'Promote awareness of the benefits of open source software and encourage its use in Angola.'
+                },
+                card_2 :{
+                    title : 'Accelerate open source development.',
+                    description : 'Offer resources and mentoring programs, workshops, and other events to help Angolan developers learn and contribute to excel in open source.'
+                },
+                card_3 :{
+                    title : 'Establish partnerships and collaborations.',
+                    description : 'To strengthen the free software community in Angola by facilitating the exchange of knowledge, resources and experiences between developers.'
+                },   
+             }
+        }
+    }
+}
+
+export { messages }
---
+++ src/translate/languages/pt.ts
@@ -0,0 +1,53 @@
+const messages = {
+    pt: {
+        translations: {
+            titles: {
+                app: 'Minha aplicação: {{name}}'
+            },
+            heading: {
+                info: 'Participe agora do primeiro evento presencial da Angola Open Source Commnunity em Luanda.',
+            },
+            nav: {
+                link_1 : 'Inicio',
+                link_2 : 'Programa de Mentoria',
+                link_3 : 'Equipa',
+                link_4 : 'Evento',
+                link_5 : 'Motivações',
+                button : 'Faça uma doação'
+             },
+             section_1:{
+                title:"Criando um impacto significativo na inovação tecnológica do país.",
+                description: 'A nossa missão é promover o desenvolvimento e adoção de Free and Open-source Software(FOSS) em Angola.',
+                button: 'Juntar-me a comunidade',
+                link: 'Saber mais',
+                info:'Junte-se a mais de 200 membros',
+                bloom_1: 'Software Livre',
+                bloom_2: 'Colaboração'
+             },
+             marquee:{
+                text_1 : "Comunidade",
+                text_2: 'Software Livre',
+                text_3: 'Colaboração'
+             },
+             section_2:{
+                title : "Nossos principais objetivos",
+                card_1 :{
+                    title : 'Fomentar a cultura do software livre.',
+                    description : 'Promover a conscientização sobre os benefícios do software de código aberto e incentivar seu uso em Angola.'
+                },
+                card_2 :{
+                    title : 'Acelerar o desenvolvimento de código aberto.',
+                    description : 'Oferecer recursos e programas de mentoria, workshops, e outros eventos para ajudar o desenvolvedor angolano aprender, e contribuir a se destacar no código aberto.'
+                },
+                card_3 :{
+                    title : 'Estabelecer parcerias e colaborações.',
+                    description : 'Fortalecer a comunidade de software livre em Angola, facilitando a troca de conhecimento, recursos e experiências entre desenvolvedores.'
+                },   
+        }
+        
+    }
+}
+}
+
+
+export { messages }
