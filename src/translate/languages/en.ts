81b15a18711e1690e63120fb2f2cb48051d7abfd
Afranioalves <afranio777alves@gmail.com>
2023-10-24 10:30:43 +0100

    feat: Adicionar suporte a multi-lingue

---
+++ src/translate/languages/en.ts
@@ -0,0 +1,128 @@
+const messages = {
+    en: {
+        translations: {
+            titles: {
+                app: 'My application: {{name}}'
+            },
+            heading: {
+                info: 'Take part now in the first face-to-face event of the Angola Open Source Commnunity in Luanda.',
+                smallText: 'A small text'
+            },
+            nav: {
+               link_1 : 'Home',
+               link_2 : 'Motivations',
+               link_3 : 'AOSC Mentorship',
+               link_4 : 'Team',
+               link_5 : 'AOSFest 2023',
+               button : 'Donate'
+            },
+            headline:{
+                title :`Creating a significant impact, on technological innovation in the country.`,
+                description : 'Our mission is to promote the development and adoption of Free and Open-source Software (FOSS) in Angola.',
+                button : 'Join the community',
+                link : 'Learn more',
+                info :'Join 200+ members',
+                bloom_1 : 'Free Software',
+                bloom_2 : 'Collaboration'
+
+             },
+             tape:{
+                text_1 : "Community",
+                text_2 : 'Free Software',
+                text_3 : 'Collaboration'
+             },
+             goal:{
+                title : "Our main goals",
+                section_1 :{
+                    title : 'Encouraging a free software culture.',
+                    description : 'Promote awareness of the benefits of open source software and encourage its use in Angola.'
+                },
+                section_2 :{
+                    title : 'Accelerate open source development.',
+                    description : 'Offer resources and mentoring programs, workshops, and other events to help Angolan developers learn and contribute to excel in open source.'
+                },
+                section_3 :{
+                    title : 'Establish partnerships and collaborations.',
+                    description : 'To strengthen the free software community in Angola by facilitating the exchange of knowledge, resources and experiences between developers.'
+                },   
+             },
+             motivation:{
+                title: "Our Motivations",
+                description:"We are guided by a set of motivations that help us to effectively achieve our mission.",
+                button: "join the community",
+                section_1:{
+                    title: "Free Software in Angola",
+                    description: "We want to increase the recognition of the free software community in Angola."
+                },
+                section_2:{
+                    title: "Open Source code in Angola",
+                    description: "Open source software is a powerful tool for a country's technological and economic development."
+                },
+                section_3:{
+                    title: "Promoting innovation and technological development",
+                    description: "By enabling Angolan developers to actively participate in this ecosystem, we hope to boost local innovation, stimulate job creation and contribute to the digital transformation of the country and the world."
+                }
+             },
+             program:{
+                title:"Mentorship Program",
+                button:"Learn about the Mentorship Program",
+                bloom:"12+ mentees",
+                section_1:{
+                    title:"Workshops",
+                    description:"It includes a week of online workshops and introductory training on Git and other essential tools for open source software development."
+                },
+                section_2:{
+                    title:"Practical Experience",
+                    description:"It connects experienced mentors, talented collaborators and free software organizations to contribute to open source projects."
+                },
+                section_3:{
+                    title:"Career development",
+                    description:"It provides a solid base of knowledge and skills for Angolan developers who wish to contribute to free software projects."
+                }
+             },
+             team:{
+                title:"Meet our amazing core team",
+                member_1:{
+                    occupation:"FOUNDER | COMPUTER SCIENCE STUDENT",
+                    description:"Collaborating researcher at the Multimodal Digital Media Analysis Lab @ IIIT-D. Passionate about Cloud Native Computing and open-source."
+                },
+                member_2:{
+                    occupation:"CO-FOUNDER | SOFTWARE DEVELOPER",
+                    description:"I'm a software developer and community leader. Passionate about sharing knowledge and enthusiastic about open source software."
+                },
+                member_3:{
+                    occupation:"CORE MEMBER | SOFTWARE DEVELOPER",
+                    description:""
+                },
+                member_4:{
+                    occupation:"CORE MEMBER | SOFTWARE DEVELOPER | UI DESIGNER",
+                    description:"Passionate about software development, design and education"
+                },
+             },
+             footer:{
+                button:"Contact us",
+                button_top:"Back to top",
+                aosc:{
+                    link_1:"Home",
+                    link_2:"Motivations",
+                    link_3:"Team",
+                },
+                community:{
+                    title:'Community'
+                },
+                project:{
+                    title:'Projects',
+                    link_1:'Mentorship Program'
+                },
+                copyright:{
+                    info_1:" All rights reserved.",
+                    info_2 : "licensed under the",
+                    link_1 : "source code",
+                    link_2 : "Apache-2.0 License",
+                }
+             }
+        }
+    }
+}
+
+export { messages }
