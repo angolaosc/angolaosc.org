899a58ff3aca266e74234b6087a2e61bf599bf3d
JesseIngles <JesseIngles@hotmail.com>
2026-04-06 17:48:57 +0100

    fix: update  members avatars

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/**',
+            }
+        ]
+    }
+};
 
 module.exports = nextConfig;
